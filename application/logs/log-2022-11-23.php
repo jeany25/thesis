6e767fbd1702c336b3243a297f46862359af603e
jeany25 <cervantesjeany2000@gmail.com>
2022-11-23 19:50:14 +0800

    first commit

---
+++ application/logs/log-2022-11-23.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-11-23 15:07:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'billingci' C:\xampp\htdocs\bill\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-11-23 15:07:09 --> Unable to connect to the database
